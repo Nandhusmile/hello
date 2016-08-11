1b4fbb2a9450ceb74681854ab96650f9873f854b
Nandhusmile <nandhu.smile11@gmail.com>
2016-08-11 14:08:14 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,24 @@
+package Logic1;
+
+public class Positive 
+{
+	public static void main(String[] args) 
+	{
+		
+		int a=10;
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("Zero");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+		
+	}
+
+}
